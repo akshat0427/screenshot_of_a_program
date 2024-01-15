306d589095fe39c724478153363c182757abf974
alexey <100063366+akshat0427@users.noreply.github.com>
2024-01-15 22:30:02 +0530

    Add files via upload

---
+++ automate_2.py
@@ -0,0 +1,45 @@
+
+from tkinter import filedialog as fd
+from win10toast import ToastNotifier
+import time
+import os
+import pygetwindow
+import pyautogui
+from PIL import Image
+
+
+def open_file_selection():
+    file = fd.askopenfile()
+    return file.name
+
+path_ = open_file_selection()
+
+os.system(f"code . {path_}")
+titles = pygetwindow.getAllTitles()
+
+
+
+for i in titles:
+    # print(i)
+    if 'Code' in i:
+        
+        vs_code_window = i
+        
+def screenshot(window):
+    
+    path = '''.\_automated_screenshot.jpg'''
+   
+    window = pygetwindow.getWindowsWithTitle(window)[0]
+    
+    left, top = window.topleft
+    right,bottom = window.bottomright
+    pyautogui.screenshot(path)
+
+    im = Image.open(path)
+    im = im.crop((left, top, right, bottom))
+    im.save(path)
+    
+time.sleep(3)
+
+screenshot(vs_code_window)
+
